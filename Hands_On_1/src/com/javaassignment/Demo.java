715844fdf81760f65b4b988c1974ee94c11e1456
Dhivesh <dhiveshyuvaraj10@gmail.com>
2026-03-09 15:11:59 +0530

    Updated file

---
+++ Hands_On_1/src/com/javaassignment/Demo.java
@@ -0,0 +1,16 @@
+package com.javaassignment;
+
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter your Name: ");
+		String name = sc.nextLine();
+		System.out.println("Enter your Age: ");
+		int age = sc.nextInt();
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+	sc.close();	
+	}
+}
